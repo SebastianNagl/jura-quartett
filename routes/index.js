769cf5eac45fc807d5e641da911db735fea367dd
psch0rr95 <sebastiannagl@icloud.com>
2020-04-27 16:34:41 +0200

    set seed route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+const Card = require("../models/Card");
+const cards = require("../content.json");
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+router.get("/seed/:password", (req, res) => {
+  if (req.params.password === process.env.SEED_PW) {
+    Card.collection.drop();
+    Card.insertMany(cards)
+      .then(() => console.log("created cards, will now close connection"))
+      .catch((err) => console.log(err));
+  }
 });
 
 module.exports = router;
