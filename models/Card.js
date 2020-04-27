f0c1033705089dcd162435b3fce355f6888842aa
psch0rr95 <sebastiannagl@icloud.com>
2020-04-27 16:24:46 +0200

    finished seed stuff

---
+++ models/Card.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const cardSchema = new Schema({
+  "105 BGB / 20 StGB - Faktor": Number,
+  Urteil: String,
+  "0-18 Examensrelevanz": Number,
+  "Längstes Wort": String,
+  "Impact in Hämmern 0-5": String,
+  Impact: String,
+  "§§ Kette": String,
+  Zitat: String,
+  Sachverhalt: String,
+});
+
+const Card = mongoose.model("Card", cardSchema);
+
+module.exports = Card;
