1b4b90b5b7b62c5b04ad6f6b7853be630fb15349
Avtandil <a.shaburishvili@gmail.com>
2020-04-27 22:37:34 +0400

    created homepage and routes

--- routes/about.js
+++ routes/about.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-/* GET home page */
-router.get("/", (req, res, next) => {
-  res.render("index");
+/* GET About Us page */
+router.get("routes/about", (req, res, next) => {
+  res.render("about");
 });
 
 module.exports = router;
